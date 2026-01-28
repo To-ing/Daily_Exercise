955782ae8671a0c6ecae4233fea401c55235725b
To-ing <tujiayin@outlook.com>
2026-01-28 16:30:50 +0800

    洛谷#108_P1036题解补充

---
+++ LuoGu_#108_P1036/108_P1036.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+typedef long long ll;
+const int Max = 1e8;
+
+int n, k;
+int cnt = 0;
+bool is_Prime[Max + 1];
+
+void sieve() {
+    is_Prime[0] = is_Prime[1] = true;
+    for (int i = 2; i * i < Max; ++i) {
+        if (!is_Prime[i]) {
+            for (int j = i * i; j < Max; j += i) {
+                is_Prime[j] = true;
+            }
+        }
+    }
+}
+
+void dfs(int id, int cnt_k, ll sum, vector<int>& x) {
+    if (cnt_k == k) {
+        if (!is_Prime[sum]) cnt++;
+        return;
+    }
+    for (int i = id; i < n; i++) {
+        dfs(i + 1, cnt_k + 1, sum + x[i], x);
+    }
+}
+
+int main() {
+    sieve();
+    cin >> n >> k;
+    vector<int> x(n);
+    for (int i = 0; i < n; i++) cin >> x[i];
+    dfs(0, 0, 0, x);
+    cout << cnt;
+    return 0;
+}
